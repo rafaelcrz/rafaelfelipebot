050b63240a6d4177f8f67df7b359c84cf115d1c5
rafaelcrz <rafael.felipe1989@gmail.com>
2017-06-25 15:38:26 -0300

    init

---
+++ bot_types.py
@@ -0,0 +1,10 @@
+
+class Commands:
+    start = 'start'
+    cv = 'cv'
+    help = 'help'
+    github = 'github'
+    contact = 'contact'
+    portfolio = 'portfolio'
+    webportfolio = 'webportfolio'
+
