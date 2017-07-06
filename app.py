14418c3f8c0672e3b4808cce04bc5c17d817e436
Rafael <rafael.felipe1989@gmail.com>
2017-07-06 12:39:17 -0300

    webook

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import random
 from api import Repositorie
 from bot_types import Commands
 import telebot
+import os
 from telebot import types
 from emoji import emojize
 
 from secrets import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
-
 me = bot.get_me()
 
 commands = Commands()
@@ -19,9 +19,16 @@ repos = Repositorie()
 # Messages
 others_messages = [
     "Ok, here's other stuff to check out",
-    "Wel, select a new theme",
+    "Well, select a new theme",
     "What about you want learning?"
 ]
+
+app = Flask(__name__)
+
+
+# "Hi! I’m RafaelFelipebot.
+# Rafael create me for help you to know some informations about him.
+# Ask me about academic or professional life and I’ll send you"
 
 
 # Options menu
@@ -117,7 +124,7 @@ def show_github(message):
 def show_education(message):
     bot.send_message(message.chat.id, "Great!")
     bot.send_message(message.chat.id,
-                     "Great! Rafael loves learning new thing! He's passionate for tecnology\nI hope that one day his make me talk " + emojize(
+                     "Great! Rafael loves learning new things! He's passionate for tecnology\nI hope that one day his make me talk " + emojize(
                          ":smile:", use_aliases=True), reply_markup=menu_education())
 
 
@@ -133,22 +140,22 @@ def show_exchange(message):
     bot.send_message(message.chat.id,
                      "Rafael got a scholarship to study in Spain. The brazilian exchange program's name is 'Ciência sem Fronteiras'")
     bot.send_message(message.chat.id, "He studied for one year 2013-2014, at 'Universidad Autónoma de Madrid'")
-    bot.send_message(message.chat.id, "In Spain he can improve his spanish.")
+    bot.send_message(message.chat.id, "In Spain he can improves his spanish.")
     show_languages(message)
 
 
 def show_contact(message):
     bot.send_message(message.chat.id,
-                     "Hey, you can send me a email, rafael.felipe1989@gmail.com")
-    bot.send_message(message.chat.id, "If you want, we can to talk on Skype, it's my nickname  rafaelfelipe.info",
+                     "Hey, it's hin email, rafael.felipe1989@gmail.com")
+    bot.send_message(message.chat.id, "If you want, add Rafael on Skype, rafaelfelipe.info",
                      disable_web_page_preview=True)
-    bot.send_message(message.chat.id, "In my Linkedin has more information and details about me https://goo.gl/bEagSw")
-    bot.send_message(message.chat.id, "I have a personal page. Do you want that I open it now? rafaelcrz.github.io")
+    bot.send_message(message.chat.id, "In Linkedin has more information and detail https://goo.gl/bEagSw")
+    bot.send_message(message.chat.id, "It's a personal page. rafaelcrz.github.io")
 
 
 # Show the languages
 def show_languages(message):
-    bot.send_message(message.chat.id, "Listen he talk ", reply_markup=menu_languages())
+    bot.send_message(message.chat.id, "Listen he talks", reply_markup=menu_languages())
 
 
 ############################################################################################
@@ -157,9 +164,8 @@ def show_languages(message):
 # User send 'start' command
 @bot.message_handler(commands=[commands.start])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Hi, I'm the personal bot of Rafael.\nI want help you learn about hin")
-    bot.send_message(message.chat.id, "So, lets start " + emojize(":smile:", use_aliases=True))
-    bot.send_message(message.chat.id, "Basically what do what you know?", disable_notification=True,
+    bot.send_message(message.chat.id, "Hi " + message.chat.first_name + emojize(":smile:", use_aliases=True))
+    bot.send_message(message.chat.id, "What do you want know about?", disable_notification=True,
                      reply_markup=menu_main_topcs())
 
 
@@ -168,6 +174,7 @@ def send_welcome(message):
 def send_sorry(message):
     bot.send_message(message.chat.id,
                      "I'm sorry, I cant talk using sentences, but I promess in my next version, I will do it.\n")
+    bot.send_message(message.chat.id, "But this is some topics for you")
     show_main_topics(message)
 
 
@@ -179,7 +186,7 @@ def test_callback(call):
             show_education(call.message)
         elif call.data == commands.carrer:
             bot.send_message(call.message.chat.id,
-                             "I was trainee in LexTrend a IT company\nAnd I'm searching for an oportunity as Android developer jr")
+                             "He was trainee in LexTrend a IT company in Madrid.\nAt now he's searching for an oportunity as Android developer jr\nHe's studing Python and Chatbots")
             bot.send_message(call.message.chat.id, "Do you want I send you his resume?",
                              reply_markup=menu_resume())
         elif call.data == commands.degree:
@@ -198,15 +205,21 @@ def test_callback(call):
         elif call.data == "exchange_no":
             show_other_topics(call.message)
         elif call.data == commands.pt:
+            bot.send_message(call.message.chat.id,
+                             "Wait a moment, I will send you the audio")
             send_audio(call.message, commands.pt)
-            bot.send_message(call.message.chat.id, "Its his native language =D \nSelect other language")
+            bot.send_message(call.message.chat.id, "It's his native language =D \nSelect other language")
             show_languages(call.message)
         elif call.data == commands.es:
+            bot.send_message(call.message.chat.id,
+                             "Wait a moment, I will send you the audio")
             send_audio(call.message, commands.es)
             bot.send_message(call.message.chat.id,
                              "A Rafael le gusta mucho el español! A ver cuando le toca volver a España")
             show_languages(call.message)
         elif call.data == commands.en:
+            bot.send_message(call.message.chat.id,
+                             "Wait a moment, I will send you the audio")
             send_audio(call.message, commands.en)
             bot.send_message(call.message.chat.id,
                              "I know his English is some basic, but he has studied very hard for improve it")
@@ -217,7 +230,8 @@ def test_callback(call):
             show_main_topics(call.message)
         elif call.data == commands.contact:
             show_contact(call.message)
-            show_other_topics(call.message)
+            show_main_topics(call.message)
+
         elif call.data == commands.github:
             show_github(call.message)
             show_other_topics(call.message)
@@ -236,17 +250,28 @@ def send_cv(message, document):
     bot.send_message(message.chat.id, "Thank's for you interess")
 
 
+# bot.polling()
+
+
 ############################################################################################
 # Flask implementation
 
-app = Flask(__name__)
+@app.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
 @app.route("/")
-def index():
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://rafaelfelipebot.herokuapp.com/bot")  # rafaelfelipebot
     bot.polling()  # none_stop=False
-    return 'It works!'
+    return "!", 200  # 'It works!'
 
 
-if __name__ == "__main__":
-    app.run()
+app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+app = Flask(__name__)
+
+# if __name__ == "__main__":
+#    app.run()
